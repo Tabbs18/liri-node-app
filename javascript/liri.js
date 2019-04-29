1d55509a8f0490be00f4f208d8b456eb9f34bc57
Tabbs18 <tabithamclaughlin500@yahoo.com>
2019-04-29 12:52:27 -0400

    Added packages as well as the axios

---
+++ javascript/liri.js
@@ -0,0 +1,28 @@
+// Basic Node application for requesting data from the OMDB website via axios
+// Here we incorporate the "axios" npm package
+var axios = require("axios");
+
+// We then run the request with axios module on a URL with a JSON
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+
+// var Spotify = require('node-spotify-api');
+ 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+ 
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
+
